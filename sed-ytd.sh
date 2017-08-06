89cd386a8a6b9e9257f764fbfa1155a590dfd497
Leroy Scandal <gitfu@users.noreply.github.com>
2017-08-06 13:55:36 -0400

    Update sed-ytd.sh

--- sed-ytd.sh
+++ sed-ytd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+## usage:  ./sed-ytd.sh 'https://www.youtube.com/watch?v=7R8OQOjqN1A'
 curl -s $1 \
 | sed -e /'ytplayer.config = {'/!d  -e '/\\u0026/ s//\n/g' -e '/,/ s//\n/g' -e '/\\/ s///g' \
 | sed -e /'itag'/!d  -e '/.*https/ s//https/g' -e /'^https'/!d  -e '/+/ s///g' -e '/%/ s//\\x/g' \
